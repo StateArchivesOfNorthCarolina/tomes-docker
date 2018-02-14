a35e434311f1af00ba447e8d8e001f9fa18c8f9d
jgibson <jeremy.gibson@ncdcr.gov>
2018-02-14 12:59:10 -0500

    Fixing tomes_tool path to corenlp-server

--- Servers/tomes_server/tomes_tool/main.py
+++ Servers/tomes_server/tomes_tool/main.py
@@ -22,10 +22,9 @@ from lib.text_to_nlp import *
 from lib.nlp_to_xml import *
 from lib.eaxs_to_tagged import *
 
-ex_dir = os.getcwd()
+ex_dir = os.path.join(os.getcwd(), "tomes_tool")
 
-
-class TOMESToolTagger:
+class TOMESToolTagger():
     """ A class to convert an EAXS file to a tagged EAXS document.
 
     Example:
